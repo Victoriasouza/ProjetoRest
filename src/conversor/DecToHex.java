4566f3b755fbd2571929cd51d2a1f4a2be534971
Victoriasouza <48362145+Victoriasouza@users.noreply.github.com>
2020-05-25 13:31:57 -0300

    Adicionando arquivos do projeto

--- src/conversor/DecToHex.java
+++ src/conversor/DecToHex.java
@@ -1,21 +1,3 @@
-/*package rest;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-
-@Path("/echo")
-public class Exemplo1 {
-	
-	@GET
-	@Produces ("text/plain")
-	public String echo (@QueryParam ("word") String palavra) {
-		return "echo: "+ palavra;
-	}
-
-}
-*/
 
 package conversor;
 
